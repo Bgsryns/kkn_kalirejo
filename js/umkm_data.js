9d10f1ac3847791954a84a7b07c01f0192c63551
Bagasady <addy.bagas@gmail.com>
2025-06-09 23:39:56 +0700

    kkn

---
+++ js/umkm_data.js
@@ -0,0 +1,44 @@
+var umkmData = [
+  {
+    name: "Toko Khanza",
+    description:
+      "Menyediakan alat tulis, ATK, dan kebutuhan sekolah. Lengkap & murah!",
+    coordinates: [-7.1405628, 110.4274942], // Koordinat langsung dari screenshot
+    link: "https://www.google.com/maps/dir/?api=1&destination=-7.137500,110.4215002", // Link Google Maps untuk rute
+  },
+  {
+    name: "Warung Makan Bu Ani",
+    description: "Aneka masakan rumahan lezat, sarapan hingga makan malam.",
+    coordinates: [-7.1408, 110.4278], // Dekat Jl. Flamboyan
+    link: "https://www.google.com/maps/dir/?api=1&destination=-7.137500,110.4215003",
+  },
+  {
+    name: "Laundry Kilat Bersih",
+    description:
+      "Jasa cuci setrika cepat dan rapi. Pakaian bersih wangi setiap hari.",
+    coordinates: [-7.1402, 110.4271], // Area sekitar Jl. Flamboyan
+    link: "https://www.google.com/maps/dir/?api=1&destination=-7.137500,110.4215004",
+  },
+  {
+    name: "Konter Pulsa & Data 'Flash Cell'",
+    description: "Jual pulsa, paket data semua operator, aksesoris handphone.",
+    coordinates: [-7.14095, 110.42735], // Masih di sekitar area KKN
+    link: "https://www.google.com/maps/dir/?api=1&destination=-7.137500,110.4215005",
+  },
+  {
+    name: "Jasa Jahit & Vermak Pak Jono",
+    description:
+      "Menerima jahit baju seragam, vermak pakaian, dan jahit kustom.",
+    coordinates: [-7.1411, 110.4276], // Dekat pemukiman warga
+    link: "https://www.google.com/maps/dir/?api=1&destination=-7.137500,110.4215006",
+  },
+  {
+    name: "Home Industri Kerupuk 'Gurih Mantap'",
+    description:
+      "Produksi kerupuk rumahan aneka rasa, cocok untuk camilan atau oleh-oleh.",
+    coordinates: [-7.1415, 110.4282], // Agak masuk ke dalam gang/area perumahan
+    link: "https://www.google.com/maps/dir/?api=1&destination=-7.137500,110.4215007",
+  },
+  // Anda bisa tambahkan lebih banyak UMKM di sini mengikuti format di atas.
+  // Jangan lupa pastikan koordinatnya sesuai dengan lokasi riil.
+];
